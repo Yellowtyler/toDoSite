2c2fec8923ac6105e28979adb59506d20ae6f765
Daniil <davydovdanya18@gmail.com>
2020-06-29 16:42:59 +0300

    -fixed POST request createProject in ProjectController.java -added org.json

--- src/main/java/com/example/toDo/entity/ERole.java
+++ src/main/java/com/example/toDo/entity/ERole.java
@@ -1,6 +1,16 @@
 package com.example.toDo.entity;
 
 public enum ERole {
-    ROLE_USER,
-    ROLE_ADMIN
+    ROLE_USER("ROLE_USER"),
+    ROLE_ADMIN("ROLE_ADMIN");
+
+    private String roleStr;
+
+    ERole(String roleStr) {
+        this.roleStr = roleStr;
+    }
+
+    public String getRoleStr() {
+        return roleStr;
+    }
 }
--- src/main/java/com/example/toDo/rest/ProjectController.java
+++ src/main/java/com/example/toDo/rest/ProjectController.java
@@ -1,28 +1,56 @@
 package com.example.toDo.rest;
 
-
 import com.example.toDo.entity.Project;
+import com.example.toDo.entity.User;
+import com.example.toDo.repository.UserRepository;
 import com.example.toDo.service.ProjectService;
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Optional;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/project")
 public class ProjectController {
+    private static final Logger logger = LoggerFactory.getLogger(ProjectController.class);
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm");
 
     @Autowired
     ProjectService projectService;
 
+    @Autowired
+    UserRepository userRepository;
+
     //@PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
     @PostMapping("/createProject")
-    public ResponseEntity<String> createProject(@RequestBody Project project) {
+    public ResponseEntity<String> createProject(@RequestBody String projectJson) {
       try {
+          JSONObject jsonObject = new JSONObject(projectJson);
+          Optional<User> user = userRepository.findByLogin(jsonObject.getString("username"));
+          Project project = new Project();
+          project.setUser(user.get());
+          project.setState(true);
+          project.setName(jsonObject.getString("name"));
+          project.setDescr(jsonObject.getString("descr"));
+          System.out.println(jsonObject.getString("date"));
+          if(jsonObject.get("date") == JSONObject.NULL) {
+              project.setDate(null);
+          } else {
+              String dateStr = jsonObject.getString("date");
+              dateStr = dateStr.substring(0, dateStr.length()-3);
+              dateStr = dateStr.replace(",","");
+              LocalDateTime localDateTime = LocalDateTime.parse(dateStr, formatter);
+              project.setDate(localDateTime);
+          }
           projectService.createProject(project);
       } catch (Exception exp) {
           return ResponseEntity.badRequest().body(exp.getMessage());
